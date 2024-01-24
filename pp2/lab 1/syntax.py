84198e2fc8042048dcfdc52259a2f540fe8525a0
kozhakhmetovi <157587450+kozhakhmetovi@users.noreply.github.com>
2024-01-24 23:14:44 +0600

    Add files via upload

---
+++ pp2/lab 1/syntax.py
@@ -0,0 +1,5 @@
+# ex1
+print("Hello World")
+
+# ex2
+print("YES")
---
+++ pp2/lab 1/variables.py
@@ -0,0 +1,32 @@
+# ex1
+carname="Volvo"
+
+# ex2
+x=50
+
+# ex3
+x = 5
+y=10
+print(x+y)
+
+# ex4
+x = 5
+y = 10
+z=x + y
+print(z)
+
+# ex5
+x,y,z="Orange","Banana","Cherry"
+
+# ex6
+x=y=z="Orange"
+
+# ex7
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
